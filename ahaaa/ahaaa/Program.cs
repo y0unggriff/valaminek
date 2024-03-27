2011afebe7c008c05c7fe4f4cb11c7c80abe5560
unknown <dunai.roland16@gmail.com>
2024-03-27 16:26:14 +0100

    added README

---
+++ ahaaa/ahaaa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ahaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= j; i++)
+            {
+                Console.Write(i);
+
+                if (i < j)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}
